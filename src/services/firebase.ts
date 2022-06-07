965fdd84a2087e98870f56aef41c1f80043cbb9a
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-06-07 16:07:54 -0300

    finished firebase integration

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCvKxV5outcQ0WHAHKQdpTLlfb-UcmRM6k",
-    authDomain: "letmeask-c9f00.firebaseapp.com",
-    databaseURL: "https://letmeask-c9f00-default-rtdb.firebaseio.com",
-    projectId: "letmeask-c9f00",
-    storageBucket: "letmeask-c9f00.appspot.com",
-    messagingSenderId: "912051906196",
-    appId: "1:912051906196:web:848ee832c3a61b794c7681"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig);
