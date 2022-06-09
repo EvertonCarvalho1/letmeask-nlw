a4d61ab4f5764431c8e021e84cafa07d194c0d0c
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-06-08 21:50:58 -0300

    building the home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import LogoImg from '../assets/images/logo.svg';
+
+export function Home() {
+    return (
+        <div>
+            <aside>
+                <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+                <strong>Crie salas de Q&amp;A ao-vovo</strong>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
+                <main>
+                    <div>
+                        <img src="" alt="" />
+                    </div>
+                </main>
+            </aside>
+        </div>
+    )
+}
