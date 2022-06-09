fa590fe043b969749bfc95cdb6a869f7172d307e
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-06-08 22:30:48 -0300

    styling the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,33 +2,36 @@ import illustrationImg from '../assets/images/illustration.svg';
 import LogoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
 
+import '../styles/auth.scss';
+
 export function Home() {
     return (
-        <div>
+        <div id='page-auth'>
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
-                <main>
-                    <div>
-                        <img src={LogoImg} alt='Letmeask' />
-                        <button>
-                            <img src={googleIconImg} alt="Logo do Google" />
-                            Crie sua sala com o Google
-                        </button>
-                        <div>ou entre em uma sala</div>
-                        <form>
-                            <input
-                                type="text"
-                                placeholder='Digite o código da sala'
-                            />
-                            <button type="submit">
-                                Entrar na sala
-                            </button>
-                        </form>
-                    </div>
-                </main>
             </aside>
+
+            <main>
+                <div>
+                    <img src={LogoImg} alt='Letmeask' />
+                    <button>
+                        <img src={googleIconImg} alt="Logo do Google" />
+                        Crie sua sala com o Google
+                    </button>
+                    <div>ou entre em uma sala</div>
+                    <form>
+                        <input
+                            type="text"
+                            placeholder='Digite o código da sala'
+                        />
+                        <button type="submit">
+                            Entrar na sala
+                        </button>
+                    </form>
+                </div>
+            </main>
         </div>
     )
 }
