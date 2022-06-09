6797a43d01f7948b979eb57605fdfd65563d315a
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-06-08 22:16:48 -0300

    styling the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,31 @@
 import illustrationImg from '../assets/images/illustration.svg';
 import LogoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
 
 export function Home() {
     return (
         <div>
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-                <strong>Crie salas de Q&amp;A ao-vovo</strong>
+                <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
                 <main>
                     <div>
-                        <img src="" alt="" />
+                        <img src={LogoImg} alt='Letmeask' />
+                        <button>
+                            <img src={googleIconImg} alt="Logo do Google" />
+                            Crie sua sala com o Google
+                        </button>
+                        <div>ou entre em uma sala</div>
+                        <form>
+                            <input
+                                type="text"
+                                placeholder='Digite o código da sala'
+                            />
+                            <button type="submit">
+                                Entrar na sala
+                            </button>
+                        </form>
                     </div>
                 </main>
             </aside>
