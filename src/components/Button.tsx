93bf92dd0186d580152f88acdaf114e012d7ed37
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-06-10 20:54:06 -0300

    styling button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,7 @@
 import { ButtonHTMLAttributes } from 'react';
 
+import '../styles/Button.scss';
+
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export function Button(props: ButtonProps) {
