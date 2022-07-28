1c29baeb3da12ff766ae604601c21d950717ef5e
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-07-28 09:01:01 -0300

    creating dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
@@ -7,20 +8,23 @@ import { AdminRoom } from './pages/AdminRoom';
 
 import { AuthContextProvider } from './contexts/AuthContext';
 
-import './styles/global.scss';
+import GlobalStyle from './styles/global';
+import light from './styles/themes/light';
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/rooms/new' component={NewRoom} />
-          <Route path='/rooms/:id' component={Room} />
-
-          <Route path='/admin/rooms/:id' component={AdminRoom} />
-        </Switch>
+        <ThemeProvider theme={light}>
+          <GlobalStyle />
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/rooms/new' component={NewRoom} />
+            <Route path='/rooms/:id' component={Room} />
+            <Route path='/admin/rooms/:id' component={AdminRoom} />
+          </Switch>
+        </ThemeProvider>
       </AuthContextProvider>
     </BrowserRouter>
   );
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,14 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primary: '#835afd',
+        segundary: '#FFF',
+
+        background: '#f8f8f8',
+        text: '#29292e'
+    },
+}
+
+//dark theme #07070A / #24272B
+
