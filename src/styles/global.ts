c57e56eb81a88f88aad0311b6eaae6c5f8593f5b
EvertonCarvalho1 <everton.carvalho.dc@gmail.com>
2022-07-28 09:31:05 -0300

    creating dark theme

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -8,8 +8,8 @@ export default createGlobalStyle`
   }
 
 body {
-    background: ${props => props.theme.colors.background};
-    color: ${props => props.theme.colors.text};
+    background: ${props => props.theme.colors.body.color1};
+    color: ${props => props.theme.colors.body.color2};
 }
 
 body,
